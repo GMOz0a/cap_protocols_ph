fd2568e1e66d0687be89346da6535f95db9e2338
GMOz0a <122027983+GMOz0a@users.noreply.github.com>
2024-08-05 21:53:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import streamlit as st
 
 breast_resection = st.Page("breast_202406.py", title = "Breast (Carcinoma)")
 colon_rectum_resection = st.Page("colon_rectum_202406.py", title = "Colon and Rectum (Resection)")
+thyroid = st.Page("thyroid_202303.py", title = "Thyroid")
+
+sections = {
+    "Breast": breast_resection,
+    "Endocrine": thyroid,
+    "GI": colon_rectum_resection,
+}
 
 pg = st.navigation ([breast_resection, colon_rectum_resection])
 st.set_page_config(page_title = "Cancer  Reporting Protocols", page_icon = ":clipboard:")
