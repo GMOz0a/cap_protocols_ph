8ce31b7c9d78a8bcf06e7579b9cd2f3c89271efa
GMOz0a <122027983+GMOz0a@users.noreply.github.com>
2024-08-05 22:03:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ colon_rectum_resection = st.Page("colon_rectum_202406.py", title = "Colon and Re
 thyroid = st.Page("thyroid_202303.py", title = "Thyroid")
 
 sections = {
-    "Breast": breast_resection,
-    "Endocrine": thyroid,
-    "GI": colon_rectum_resection,
+    "Breast": [breast_resection],
+    "Endocrine": [thyroid],
+    "GI": [colon_rectum_resection],
 }
 
 pg = st.navigation (sections)
