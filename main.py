aa27d800393ca0c3d973e232a0a589e215d4c1b9
GMOz0a <122027983+GMOz0a@users.noreply.github.com>
2024-08-11 14:48:06 +0800

    Update main.py
    Add gynecologic section

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import streamlit as st
 
+#Home
+home_page = st.Page("home.py", title = "Home", icon="🏚️")
+
+#Breast 
 breast_resection = st.Page("breast_202406.py", title = "Breast (Carcinoma)")
-colon_rectum_resection = st.Page("colon_rectum_202406.py", title = "Colon and Rectum (Resection)")
+
+#Endocrine
 thyroid = st.Page("thyroid_202303.py", title = "Thyroid")
 
+#GI
+colon_rectum_resection = st.Page("colon_rectum_202406.py", title = "Colon and Rectum (Resection)")
+
+#Gynecologic
+cx_resection = st.Page("cx_april2023.py", title = "Cervix (Resection)")
+endom = st.Page("endom_2023.py", title = "Endometrium")
+ovary = st.Page("ovary_jun2024.py", title = "Ovary")
+uterus_sarc = st.Page("uterus_sarc_mar2022.py", title = "Uterine Sarcoma")
+gtn = st.Page("gtn_nov2021.py", title = "Trophoblastic Tumors")
+
 sections = {
+    "Home": [home_page],
     "Breast": [breast_resection],
     "Endocrine": [thyroid],
     "GI": [colon_rectum_resection],
-}
+    "Gynecologic": [cx_resection, endom, ovary, uterus_sarc, gtn]
+} 
 
-pg = st.navigation(sections)
-st.set_page_config(page_title = "Cancer  Reporting Protocols", page_icon = ":clipboard:")
+pg = st.navigation (sections)
+st.set_page_config(page_title = "The Gabberbot CAP Protocols app", page_icon = ":clipboard:")
 pg.run()
