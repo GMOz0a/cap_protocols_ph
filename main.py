49884a99daa3b968ab40610a5c0ad6dadd943647
GMOz0a <122027983+GMOz0a@users.noreply.github.com>
2024-08-24 23:00:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ gtn = st.Page("gtn_nov2021.py", title = "Trophoblastic Tumors")
 # GU
 kidney_res = st.Page("kidney_jun2024.py", title = "Kidney (Resection)")
 pros_core = st.Page("proscore_sep2023.py", title = "Prostate - Core Biopsy")
-pros_turp = st.Page("proturp_sep2023.py", title = "Prostate - Core Biopsy")
+pros_turp = st.Page("proturp_sep2023.py", title = "Prostate - TURP")
 
 #Pediatric 
 exgct = st.Page("exgct_sep2023.py", title = "Extragonadal Germ Cell Tumor (Resection)")
