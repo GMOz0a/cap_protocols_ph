49e98275819407cb60eb63d27426785035e5a3a1
GMOz0a <122027983+GMOz0a@users.noreply.github.com>
2024-10-24 21:59:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ ampulla = st.Page("ampulla_nov2021.py", title = "Ampulla of Vater")
 anus = st.Page("anus_sep2023.py", title = "Anus")
 appendix =  st.Page("appendix_dec2022.py", title = "Appendix") 
 colon_rectum_resection = st.Page("colon_rectum_202406.py", title = "Colon and Rectum (Resection)")
+dehb = st.Page("dehb_jun2021.py", title = "Distal Extrahepatic Bile Ducts")
 esophagus = st.Page("esophagus_jun2022.py", title = "Esophagus")
 gallbladder = st.Page("gb_jun2021.py", title = "Gallbladder")
-gist = st.Page("gist_sep2023.py", title = "Gist (Resection)")
+gist = st.Page("gist_sep2023.py", title = "GIST (Resection)")
 hcc = st.Page("HCC_Jun2022.py", title = "Hepatocellular Carcinoma")
+ihb = st.Page("ihb_jun2021.py", title = "Intrahepatic Bile Ducts")
+phb = st.Page("phb_jun2021.py", title = "Perihilar Bile Ducts")
 ex_panc = st.Page("expan_nov2021.py", title = "Pancreas (Exocrine)")
 stomach = st.Page("stomach_mar2023.py", title = "Stomach")
 
@@ -57,8 +60,8 @@ sections = {
     "Home": [home_page],
     "Breast": [breast_resection, phyllodes],
     "Endocrine": [thyroid, adrenal],
-    "GI": [ampulla, anus, appendix, colon_rectum_resection, 
-           esophagus, gallbladder, gist, hcc, ex_panc, stomach],
+    "GI": [ampulla, anus, appendix, colon_rectum_resection, dehb,
+           esophagus, gallbladder, gist, hcc,ihb, ex_panc, phb, stomach],
     "GU": [bladder, kidney_res, pros_core, pros_resection, pros_turp],
     "Gynecologic": [cx_bx, cx_resection, endom, ovary, uterus_sarc, vulva, gtn],
     "Head and Neck": [larynx, oral, salivary],
